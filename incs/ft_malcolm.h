0038afd301ff226bbb16a1d6e43cf4dc6a31398a
benjamin <besellem@student.42.com>
2022-08-11 15:31:23 +0200

    init

---
+++ incs/ft_malcolm.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_malcolm.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: besellem <besellem@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/11 15:24:19 by besellem          #+#    #+#             */
+/*   Updated: 2022/08/11 15:25:23 by besellem         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_MALCOLM_H
+# define FT_MALCOLM_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <errno.h>
+
+
+#endif
